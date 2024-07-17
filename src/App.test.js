8c0534d82e20028be2ac0c205fda2ccba8cc91ad
AngelLam1130 <anqi_lin1130@163.com>
2024-07-16 20:25:17 -0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders learn react link', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+  });
+  const linkElement = screen.getByText((content, element) => {
+    return content.match(/learn react/i);
+  });
   expect(linkElement).toBeInTheDocument();
 });
